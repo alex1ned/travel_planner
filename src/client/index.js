e083dba9319e91de55e269155247d6bc90d46bda
Alexander Nederegger <an@MacBook-Pro.localdomain>
2020-11-04 20:52:53 +0100

    Subject: Added basic styles (colors, button) in scss. Added colors for h1 and h2, background colors for button, footer, body, and header. These are stored as global variables in scss. More styling to be added on the go. Next up is creating the raw html structure (i.e. description, from-fields, buttons) - once this is complete I will create test functions for the three APIs and then implement a formHandler function.

--- src/client/index.js
+++ src/client/index.js
@@ -4,6 +4,11 @@ import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
 // Import styles
+import './styles/resets.scss';
+import './styles/base.scss';
+import './styles/header.scss';
+import './styles/main.scss';
+import './styles/footer.scss';
 
 // Export functions
 
